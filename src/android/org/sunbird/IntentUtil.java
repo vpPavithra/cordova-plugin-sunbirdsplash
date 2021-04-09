5c0745b5d21b634ef58e2e154c10fcb3e486077f
ShuvranilSaha <subranil.saha@gmail.com>
2021-04-09 10:38:56 +0530

    Issue #SB-23813 feat: added vendor app integration

---
+++ src/android/org/sunbird/IntentUtil.java
@@ -0,0 +1,58 @@
+package org.sunbird;
+
+import android.os.Bundle;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class IntentUtil {
+
+    public static JSONObject toJsonObject(Bundle bundle) {
+        //  Credit: https://github.com/napolitano/cordova-plugin-intent
+        try {
+            return (JSONObject) toJsonValue(bundle);
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Cannot convert bundle to JSON: " + e.getMessage(), e);
+        }
+    }
+
+    private static Object toJsonValue(final Object value) throws JSONException {
+        //  Credit: https://github.com/napolitano/cordova-plugin-intent
+        if (value == null) {
+            return null;
+        } else if (value instanceof Bundle) {
+            final Bundle bundle = (Bundle) value;
+            final JSONObject result = new JSONObject();
+            for (final String key : bundle.keySet()) {
+                result.put(key, toJsonValue(bundle.get(key)));
+            }
+            return result;
+        } else if ((value.getClass().isArray())) {
+            final JSONArray result = new JSONArray();
+            int length = Array.getLength(value);
+            for (int i = 0; i < length; ++i) {
+                result.put(i, toJsonValue(Array.get(value, i)));
+            }
+            return result;
+        } else if (value instanceof ArrayList<?>) {
+            final ArrayList arrayList = (ArrayList<?>) value;
+            final JSONArray result = new JSONArray();
+            for (int i = 0; i < arrayList.size(); i++)
+                result.put(toJsonValue(arrayList.get(i)));
+            return result;
+        } else if (
+                value instanceof String
+                        || value instanceof Boolean
+                        || value instanceof Integer
+                        || value instanceof Long
+                        || value instanceof Double) {
+            return value;
+        } else {
+            return String.valueOf(value);
+        }
+    }
+}
