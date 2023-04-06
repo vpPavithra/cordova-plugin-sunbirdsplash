c24eba1fa751115d803438da8a5b2db0634f6c69
Ajoymaity <ajoy.maity@tarento.com>
2023-04-06 18:27:51 +0530

    Issue #ED-1339 feat: Update Splash plugin and remove splash related dependency

--- www/plugin.js
+++ www/plugin.js
@@ -3,12 +3,6 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'splashscreen';
 
 var splash = {
-  show: function () {
-    exec(null, null, PLUGIN_NAME, "show", []);
-  },
-  hide: function () {
-    exec(null, null, PLUGIN_NAME, "hide", []);
-  },
   setContent: function (appName, url) {
     exec(null, null, PLUGIN_NAME, "setContent", [appName, url]);
   },
